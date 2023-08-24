fc196836553b1282d933974781876bd67f3651ae
Dubier_Barrientos <129345580+Dubier-Barrientos@users.noreply.github.com>
2023-08-24 10:46:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -51,3 +51,11 @@ elif in_mod == "Visual":
 elif in_mod == "Háptico":
   set_mod = "Activar vibración"
 st.write("La acción es:", set_mod)
+
+
+with st.sidebar:
+  st.subheader("Configura la modalidad")
+  mod_radio = st.radio(
+    "Escoge la modalidad a usar",
+    ("Visual", "Auditiva", "Haptica")
+  )
