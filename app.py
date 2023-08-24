a218c1306d41c10fcdee521c453f94bb275ea2a5
Dubier_Barrientos <129345580+Dubier-Barrientos@users.noreply.github.com>
2023-08-24 09:55:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
+from PIL import Image
 
 st.title('La aplicación más chimba')
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Fácilmente puedo realizar backend y frontend")
+image = Image.open('gatito_mojado.jpg')
+
+st.image(image, caption="Aquí va un gato mojado")
